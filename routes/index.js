090e297e23c82c97a0378cb8ceb010799abde603
Simon Fish <si@mon.fish>
2018-02-21 13:37:59 +0000

    Stats frontend, slides, portfolio fixes, etc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Simon Fish' });
+  try {
+    const portfolioYaml = yaml.safeLoad(fs.readFileSync('_data/portfolio.yml', 'utf8'));
+    const slidesYaml = yaml.safeLoad(fs.readFileSync('_data/slides.yml', 'utf8'));
+    const statsQuery = [{"fact": "this is a test", "number": 42, "icon": "fa-code-fork"}, {"fact": "this is a test", "number": 42, "icon": "fa-code-fork"}]
+    console.log(portfolioYaml)
+    res.render('index', { title: 'Simon Fish', portfolio: portfolioYaml, slides: slidesYaml, stats: statsQuery });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 module.exports = router;
