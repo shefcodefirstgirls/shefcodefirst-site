9d5b66ceea1394ae15280175ef464f2f10f9baff
Simon Fish <si@mon.fish>
2018-02-22 16:57:57 +0000

    Further changes

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,75 @@ var express = require('express');
 var router = express.Router();
 const yaml = require('js-yaml');
 const fs = require('fs');
+const axios = require('axios'); 
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN; 
+if (!GITHUB_TOKEN) throw new Error('Supply GitHub token'); 
+const graphql = `{
+  viewer {
+    organizations(last: 100) {
+      edges {
+        node {
+          name
+          url
+          avatarUrl
+        }
+      }
+    }
+    contributedRepositories(last: 100) {
+      totalCount
+    }
+    starredRepositories(last: 100) {
+      totalCount
+    }
+    issueComments {
+      totalCount
+    }
+    isHireable
+    isCampusExpert
+  }
+}`;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  try {
-    const portfolioYaml = yaml.safeLoad(fs.readFileSync('_data/portfolio.yml', 'utf8'));
-    const slidesYaml = yaml.safeLoad(fs.readFileSync('_data/slides.yml', 'utf8'));
-    const statsQuery = [{"fact": "this is a test", "number": 42, "icon": "fa-code-fork"}, {"fact": "this is a test", "number": 42, "icon": "fa-code-fork"}]
-    console.log(portfolioYaml)
-    res.render('index', { title: 'Simon Fish', portfolio: portfolioYaml, slides: slidesYaml, stats: statsQuery });
-  } catch (e) {
-    console.log(e);
-  }
+  axios.post('https://api.github.com/graphql',
+    { query: graphql }, { headers: {'Authorization': `Bearer ${GITHUB_TOKEN}`} })
+    .then((graphqlRes) => {
+      console.log(JSON.stringify(graphqlRes.data.data.viewer))
+      const simon = graphqlRes.data.data.viewer
+      const organizations = simon.organizations.edges.map(edge => edge.node);
+      const contributedRepositories = simon.contributedRepositories.totalCount
+      const starredRepositories = simon.starredRepositories.totalCount
+      const issueComments = simon.issueComments.totalCount
+      const isHireable = simon.isHireable
+      const isCampusExpert = simon.isCampusExpert
+      const portfolioYaml = yaml.safeLoad(fs.readFileSync('_data/portfolio.yml', 'utf8'));
+      const slidesYaml = yaml.safeLoad(fs.readFileSync('_data/slides.yml', 'utf8'));
+      const statsQuery = [
+        { "fact": "repositories contributed to", 
+          "number": contributedRepositories, 
+          "icon": "fa-code-fork"
+        }, 
+        { "fact": "repositories starred", 
+          "number": starredRepositories, 
+          "icon": "fa-star"
+        }, 
+        { "fact": "comments made on issues", 
+          "number": issueComments, 
+          "icon": "fa-comments"
+        } 
+      ]
+      res.render('index', { 
+        title: 'Simon Fish', 
+        portfolio: portfolioYaml, 
+        slides: slidesYaml, 
+        stats: statsQuery,
+        isHireable: isHireable
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.render('error', {});
+    });
 });
 
 module.exports = router;
